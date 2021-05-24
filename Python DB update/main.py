df51bd08ad0511be1d00f15384400e81139e1fdd
kjlee <kijeong08@gmail.com>
2021-05-24 16:36:25 +0900

    deleted ' '

--- Python DB update/main.py
+++ Python DB update/main.py
@@ -3,6 +3,8 @@ import csv
 
 file = open("hospital.csv", "r", encoding='utf8')
 conn = pymysql.connect(host='mysql-teamproject.ckwr51x4irqq.us-east-2.rds.amazonaws.com', user='user', password='asd0123!', db='jsp_teamproject', charset='utf8')
+# conn = pymysql.connect(host='127.0.0.1', user='root', password='1234', db='spro', charset='utf8')
+
 curs = conn.cursor()
 
 sql = "insert into hospital2 " \
@@ -18,9 +20,12 @@ for index,line in enumerate(rd):
          continue
       try:
          row = str(line).split(',')
-         str1 = row[0].replace("['", "")
-         str2 = row[4].replace("]", "")
-         curs.execute(sql, ( str1.replace("'",""), row[1].replace("'",""), row[2].replace("'",""), row[3].replace("'",""), str2.replace("'","") ))
+         curs.execute(sql, ( row[0].replace("['","").replace("'",""),
+                             row[1].replace(" ","").replace("'",""),
+                             row[2].replace(" ", "").replace("'",""),
+                             row[3].replace(" ", "").replace("'",""),
+                             row[4].replace("]","").replace(" ", "").replace("'","") ))
+         # curs.execute(sql, (row[0], row[1], row[2], row[3], row[4]) )
          print(row[0])
          conn.commit()
       except Exception as e:
