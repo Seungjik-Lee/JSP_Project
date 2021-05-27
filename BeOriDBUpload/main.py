006892d8f00f032b054aa0b072383c58b3fb8fad
kjlee <kijeong08@gmail.com>
2021-05-27 15:46:35 +0900

    DB Upload 수정
    idx 자동생성, 원본 파일대로 파이썬으로 통합해서 올림

---
+++ BeOriDBUpload/main.py
@@ -0,0 +1,11 @@
+from unit1 import read_csv_write_unit1
+from unit2 import read_csv_write_unit2
+from unit3 import read_csv_write_unit3
+from unit4 import read_csv_write_unit4
+
+if __name__ == '__main__':  # int main()과 같음
+
+    read_csv_write_unit1()
+    read_csv_write_unit2()
+    read_csv_write_unit3()
+    read_csv_write_unit4()
---
+++ BeOriDBUpload/unit4.py
@@ -0,0 +1,31 @@
+import csv
+import pymysql
+
+def read_csv_write_unit4():
+    file = open("info/건강보험심사평가원_전문병원 지정 현황_20210101.csv", "r", encoding='utf8')
+    conn = pymysql.connect(host='mysql-teamproject.ckwr51x4irqq.us-east-2.rds.amazonaws.com', user='user', password='asd0123!', db='jsp_teamproject', charset='utf8')
+    #conn = pymysql.connect(host='127.0.0.1', user='root', password='1234', db='spro', charset='utf8')
+    curs = conn.cursor()
+
+    sql = "insert into hospital2 " \
+          "( unit, grp, name, addr ) " \
+          "VALUES " \
+          "( %s, %s, %s, %s ) ";
+
+    rd = csv.reader(file)
+
+    for index, line in enumerate(rd):
+        if index == 0:
+            continue
+        row = str(line).split(',')
+        try:
+            curs.execute(sql, (row[0].replace("['","").replace("'",""),
+                               row[1].replace(" '","").replace("'",""),
+                               row[2].replace(" '","").replace("'",""),
+                               row[3].replace("']","").replace(" '","").replace("'","")))
+            print(row[0])
+            conn.commit()
+        except Exception as e:
+            print(e)
+            continue
+    file.close()
